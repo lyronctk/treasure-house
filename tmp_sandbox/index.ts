238b0d35e4e786d8097c52ad4931e47261ba27ec
Lyron Co Ting Keh <lyronctk@stanford.edu>
2022-09-20 14:02:40 -0700

    simple w/ curve curve25519

--- tmp_sandbox/index.ts
+++ tmp_sandbox/index.ts
@@ -1,55 +1,42 @@
-// used as reference: https://github.com/AztecProtocol/aztec-crypto-js/blob/master/bn128/bn128.js
-
-import { getMaxListeners } from "process";
-import { BindingName } from "typescript";
-
 const BN = require("bn.js");
 const EC = require("elliptic");
 const crypto = require("crypto");
 
-const GRUMPKIN_PARAMS = {
-    a: "0",
-    b: "3",
-    fieldModulus: new BN(
-        "21888242871839275222246405745257275088696311157297823662689037894645226208583",
-        10
-    ),
-    groupModulus: new BN(
-        "21888242871839275222246405745257275088548364400416034343698204186575808495617",
-        10
-    ),
-    generator: ["1", "2"],
-};
+interface Deposit {
+    // ρ * G
+    P: any;
 
-const grumpkin = {
-    curve: new EC.curve.short({
-        a: GRUMPKIN_PARAMS.a,
-        b: GRUMPKIN_PARAMS.b,
-        p: GRUMPKIN_PARAMS.fieldModulus.toString(16),
-        n: GRUMPKIN_PARAMS.groupModulus.toString(16),
-        gRed: false,
-        g: GRUMPKIN_PARAMS.generator,
-    }),
-    groupReduction: BN.red(GRUMPKIN_PARAMS.groupModulus),
-};
+    // ρ * managerPk
+    Q: InstanceType<typeof BN>;
 
-function initTreasury() {
-    const secretKey = new BN(crypto.randomBytes(32), 16).toRed(
-        grumpkin.groupReduction
-    );
-    const gx = grumpkin.curve.g.getX().toRed(grumpkin.groupReduction);
-    const gy = grumpkin.curve.g.getY();
-
-    // console.log(grumpkin.curve.red);
-    console.log(grumpkin.groupReduction);
-    console.log(gx.toString(16));
-
-    // const testSecret = new BN(3).toRed(grumpkin.groupReduction);
-    // console.log(testSecret);
-
-    // const pubKey = gx.redMul(testSecret);
-
-    // console.log(pubKey.toString(16));
+    // amount of ETH
+    v: InstanceType<typeof BN>;
 }
 
-initTreasury();
+const curve = new EC.ec("curve25519");
+
+// CREATE TREASURY
+const manager = curve.genKeyPair();
+const managerPriv = manager.getPrivate();
+const managerPub = manager.getPublic();
+console.log("Manager publishes pk:", managerPub.getX().toString(16));
+
+// DEPOSIT
+const contributor = curve.genKeyPair();
+const dep: Deposit = {
+    P: contributor.getPublic(),
+    Q: contributor.derive(managerPub),
+    v: 123,
+};
+console.log("Contributor sends in a deposit:", [
+    dep.P.getX().toString(16),
+    dep.Q.toString(16),
+    dep.v,
+]);
+
+// WITHDRAW
+const recoverShared = manager.derive(dep.P);
+console.log(
+    "Manager recovers Q using P & priv key:",
+    recoverShared.toString(16)
+);
