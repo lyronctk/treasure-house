a9dfe6b4dfd828392666d6d9a7553b88078c76ef
Lyron Co Ting Keh <lyronctk@stanford.edu>
2022-11-01 17:03:21 -0700

    Added documentation for create.ts

--- cli/create.ts
+++ cli/create.ts
@@ -1,28 +1,22 @@
+/*
+ * Creates a new private treasury by 1) generating a babyjubjub keypair for 
+ * the manager and 2) posting the public key with a label on the contract's 
+ * directory. Also contains utility func for getting an overview of the 
+ * directory. 
+ */
+
 import dotenv from "dotenv";
+dotenv.config();
 
 import { BN } from "bn.js";
 import { Bytes32 } from "soltypes";
 import { ethers } from "ethers";
-const { PublicKey, PrivateKey, Point } = require("babyjubjub");
-
-dotenv.config();
+const { PublicKey, PrivateKey } = require("babyjubjub");
 
 const CONTRACT_ADDR: string = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
 const CONTRACT_ABI =
     require("../contracts/out/PrivateTreasury.sol/PrivateTreasury.json").abi;
-
 const TREASURY_LABEL: string = "test";
-
-interface Deposit {
-    // ρ * G
-    P: InstanceType<typeof Point>;
-
-    // ρ * managerPk
-    Q: InstanceType<typeof Point>;
-
-    // amount of ETH
-    v: InstanceType<typeof BN>;
-}
 
 const signer = new ethers.Wallet(
     <string>process.env.MANAGER_ETH_PRIVKEY,
@@ -34,6 +28,11 @@ const privateTreasury = new ethers.Contract(
     signer
 );
 
+/*
+ * Converts a BigNumber to Bytes32 by turning into a hex string first. 
+ * Intermediate step is important to pad 0's on the left of the hex 
+ * representation of n until it is exactly 32 bytes. 
+ */
 function bigNumToBytes32(n: InstanceType<typeof BN>): Bytes32 {
     return new Bytes32(
         ethers.utils.hexZeroPad(
@@ -43,14 +42,37 @@ function bigNumToBytes32(n: InstanceType<typeof BN>): Bytes32 {
     );
 }
 
+/*
+ * Enumerates treasury info stored in the contract's directory. 
+ */
+async function enumerateDirectory() {
+    const dirLen = await privateTreasury.getDirectoryLength();
+    console.log(
+        `== Current directory before adding new treasury (len = ${dirLen})`
+    );
+    for (var i = 0; i < dirLen.toNumber(); i++) {
+        const entry = await privateTreasury.directory(i);
+        console.log(entry["label"], ":", [entry["pk"]["x"], entry["pk"]["y"]]);
+    }
+    console.log("==");
+}
+
+/*
+ * Generates a new keypair. Private key must be stored for later use by the 
+ * manager. Public key & a succinct label posted on-chain for contributors 
+ * to access. 
+ */
 async function createTreasury() {
     const managerPriv = new PrivateKey(PrivateKey.getRandObj().field);
     const managerPub = PublicKey.fromPrivate(managerPriv);
 
-    console.log("== Sending tx to create treasury")
+    console.log("== Sending tx to create treasury");
+    console.log("Manager's priv:", [
+        managerPriv.s.n.toString(10),
+    ]);
     console.log("Manager's pub:", [
-        managerPub.p.x.n.toString(16),
-        managerPub.p.y.n.toString(16),
+        managerPub.p.x.n.toString(10),
+        managerPub.p.y.n.toString(10),
     ]);
     const res = await privateTreasury.create(
         bigNumToBytes32(managerPub.p.x.n).toString(),
@@ -61,19 +83,7 @@ async function createTreasury() {
     console.log("==");
 }
 
-async function getDirectory() {
-    const dirLen = await privateTreasury.getDirectoryLength();
-    console.log(
-        `== Current directory before adding new treasury (len = ${dirLen})`
-    );
-    for (var i = 0; i < dirLen.toNumber(); i++) {
-        const entry = await privateTreasury.directory(i);
-        console.log(entry["label"], ':', [entry["pk"]["x"], entry["pk"]["y"]]);
-    }
-    console.log("==");
-}
-
 (async () => {
-    await getDirectory();
+    await enumerateDirectory();
     await createTreasury();
 })();
